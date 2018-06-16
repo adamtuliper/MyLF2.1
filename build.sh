9aad3ebce398c040b36213af6238f375b82bd1c4
adamtuliper <adam.tuliper@gmail.com>
2018-06-16 19:22:35 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/MyLF2.1/*.csproj && dotnet build src/MyLF2.1/*.csproj
---
+++ src/MyLF2.1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyLF2._1
+{
+    public class Class1
+    {
+    }
+}
